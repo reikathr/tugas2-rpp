1a38a3a3838f7805227e459c1d7cea32fb608ee1
meteoreinger <reikathr@gmail.com>
2025-03-20 11:29:51 +0700

    add skeleton for reasoning, subject to change

---
+++ reasoning.py
@@ -0,0 +1,12 @@
+def solve(kb, query):
+    #TODO: write SLD backward-chaining
+    pass
+
+def main():
+    #TODO: write the main code
+    # load input by file
+    # process input into chosen format
+    # convert input to cnf
+    # call solve(kb, query)
+    # print result
+    pass
