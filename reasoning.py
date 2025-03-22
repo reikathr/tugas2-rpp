6c75617ffec00e9a8b88ab48d3b5cf0f10634431
Fikri Budianto <budf03@gmail.com>
2025-03-22 21:47:55 +0700

    add backward chaining procedure

--- reasoning.py
+++ reasoning.py
@@ -1,6 +1,25 @@
 def solve(kb, query):
-    #TODO: write SLD backward-chaining
-    pass
+    """
+    SLD resolution using backward chaining for CNF clauses.
+    :param kb: List of clauses (each clause is a list of literals).
+    :param query: List of literals to prove.
+    :return: True if query is entailed by KB, else False.
+    """
+    if not query:
+        return True  # Base case: If query is empty, we have proven all goals
+
+    q = query[0]  # Take the first goal
+    rest_query = query[1:]  # Remaining goals
+
+    # Try to resolve q using KB
+    for clause in kb:
+        if q in clause:  # If q appears in a clause, try resolving its subgoals
+            new_query = [lit for lit in clause if lit != q] + rest_query
+            if solve(kb, new_query):  # Recursively check if the new query can be solved
+                return True
+
+    return False  # If no resolution leads to success, return False
+
 
 def main():
     #TODO: write the main code
